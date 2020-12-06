3cb3378676f1a3dca9d3e8fc649dccf3825d11e9
Haran <haran@nxdomain.ca>
2020-12-06 06:13:55 -0500

    Add group controller and views

---
+++ Notes/Controllers/GroupsController.cs
@@ -0,0 +1,161 @@
+﻿/** GroupsController.cs
+ * 
+ * TODO
+ * 
+ * Author: Haran
+ * Date: December 6th, 2020
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public GroupsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Groups
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Groups.Include(m => m.Notes).ToListAsync());
+        }
+
+        // GET: Groups/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @group = await _context.Groups
+                .Include(m => m.Notes) // include the notes related to this group
+                .ThenInclude(n => n.User) // include the users as well
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return View(@group);
+        }
+
+        // GET: Groups/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Groups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Group @group)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@group);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@group);
+        }
+
+        // GET: Groups/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @group = await _context.Groups.FindAsync(id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+            return View(@group);
+        }
+
+        // POST: Groups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Group @group)
+        {
+            if (id != @group.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@group);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GroupExists(@group.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@group);
+        }
+
+        // GET: Groups/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @group = await _context.Groups
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return View(@group);
+        }
+
+        // POST: Groups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @group = await _context.Groups.FindAsync(id);
+            _context.Groups.Remove(@group);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GroupExists(int id)
+        {
+            return _context.Groups.Any(e => e.Id == id);
+        }
+    }
+}
