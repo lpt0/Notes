3ee8c9df40fa926a0bbcb06d169234d8923a5f38
Haran <haran@nxdomain.ca>
2020-12-06 03:48:42 -0500

    Add views for notes and users

---
+++ Notes/Controllers/NotesController.cs
@@ -0,0 +1,173 @@
+﻿/** NotesController.cs
+ * 
+ * TODO
+ * 
+ * Author: Haran
+ * Date: December 6th, 2020
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    public class NotesController : Controller
+    {
+        // the context is what allows the controller to communicate with the database
+        private readonly ApplicationContext _context;
+
+        public NotesController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notes
+        public async Task<IActionResult> Index()
+        {
+            var applicationContext = _context.Notes.Include(n => n.Group).Include(n => n.User);
+            return View(await applicationContext.ToListAsync());
+        }
+
+        // GET: Notes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var note = await _context.Notes
+                .Include(n => n.Group)
+                .Include(n => n.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return View(note);
+        }
+
+        // GET: Notes/Create
+        public IActionResult Create()
+        {
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Username");
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Content,Description,UserId,GroupId")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(note);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Name", note.GroupId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Username", note.UserId);
+            return View(note);
+        }
+
+        // GET: Notes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var note = await _context.Notes.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Name", note.GroupId);
+            // removed the user id from view data, since I don't want that to be editable
+            return View(note);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Content,Description,GroupId")] Note note)
+        {
+            if (id != note.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(note);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NoteExists(note.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Name", note.GroupId);
+            // again, no user id since I don't want it to be edited
+            return View(note);
+        }
+
+        // GET: Notes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var note = await _context.Notes
+                .Include(n => n.Group)
+                .Include(n => n.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return View(note);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NoteExists(int id)
+        {
+            return _context.Notes.Any(e => e.Id == id);
+        }
+    }
+}
